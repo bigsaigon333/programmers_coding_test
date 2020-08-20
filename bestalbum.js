504a5e0ac0f91ed3c3b675bb565a18c3ea987eb3
bigsaigon333 <likepeppermint@gmail.com>
2020-08-21 00:00:10 +0900

    해시 문제 풀이 추가

--- bestalbum.js
+++ bestalbum.js
@@ -1,37 +1,53 @@
+/* 
+Date: 2020-08-20 Thu
+time: 28m 56s >> 통과
+Comment:
+1. 수많은 도전 끝에 드디어 해내다. 사실 로직이 어려운 건 아닌데, 이걸 자료구조로 어떻게 구현하는 지가 관건인 문제
+2. 어찌어찌 배열과 객체를 이용하여 풀었는데, 이게 최선인가 라는 의문이 남음
+ */
+
 function solution(genres, plays) {
 	const answer = [];
 
-	const album = new Map();
+	const MX = Array.from(new Set(genres)).length;
+	const key = [];
+	const value = new Array(MX).fill(0).map(() => new Array());
 
 	for (let i = 0; i < genres.length; i++) {
-		if (album.get(genres[i]) === undefined) {
-			album.set(genres[i], [
-				{
-					index: i,
-					numPlay: plays[i],
-				},
-			]);
+		let idx;
+		if (key.includes(genres[i])) {
+			idx = key.indexOf(genres[i]);
 		} else {
-			album.get(genres[i]).push({
-				index: i,
-				numPlay: plays[i],
-			});
+			key.push(genres[i]);
+			idx = key.length - 1;
 		}
-		// console.log(album);
+		value[idx].push({ play: plays[i], i });
 	}
 
-	album.forEach((value, key) => {
-		value.sort((a, b) =>
-			a.numPlay === b.numPlay ? a.index > b.index : a.numPlay < b.numPlay
-		);
-	});
+	const nKey = [];
 
-	console.log(album);
+	value.forEach((arr, i) => {
+		arr.sort((obj1, obj2) => obj2.play - obj1.play);
+		nKey.push({
+			genre: key[i],
+			sum: arr.reduce((acc, curr) => (acc += curr.play), 0),
+			i,
+		});
+	});
+	nKey.sort((obj1, obj2) => obj2.sum - obj1.sum);
+	// console.table(nKey);
+	// console.table(value);
+
+	for (let i = 0; i < nKey.length; i++) {
+		answer.push(value[nKey[i].i][0].i);
+		if (value[nKey[i].i].length > 1) answer.push(value[nKey[i].i][1].i);
+	}
+	// console.log(answer);
 
 	return answer;
 }
 
 solution(
-	["pop", "classic", "pop", "pop", "classic"],
+	["pop", "classic", "pop", "pop", "SexyDance"],
 	[500, 600, 150, 800, 2500]
 );
